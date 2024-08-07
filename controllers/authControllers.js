7c6e590be0c1c0ea1b978cd323a2e27468d51387
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-07 15:59:34 +0530

    add foodapp master

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,28 @@
 //REGISTER
 
-const registerController =() => {}
+const userModels = require("../models/userModels")
+
+const registerController = async (req,res) => {}
+try{
+    const {username,email,password,phone,address} = req.body//
+      //validation
+      if(!username ||!email || !password || !address || !phone){
+        return res.status(500).send({
+            success:false,
+            message:"Please fill all the fields"
+        })
+      }
+
+      //check user
+      const exisiting = await userModel.findOne({email})
+      
+}catch (error){
+    console.log(error)
+    res.status(500).send({
+        success:false,
+        message:'Error in register api',
+        error
+    })
+};
 
 module.exports = {registerController}
