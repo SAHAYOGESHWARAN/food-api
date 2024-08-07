96073de8263088ba0f8f7aab3cf11bce2d973ddb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-07 15:31:14 +0530

    add foodapp master

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { registerController } = require('../controllers/authControllers')
 
-const router = express.router()
+const router = express.Router()
 
 //routes
 //REGISTER || POST
