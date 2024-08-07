b5c190485d559c09c9f0980d93b79289a207766c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-07 16:44:43 +0530

    add foodapp master

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,40 +1,42 @@
-//REGISTER
+const userModels = require("../models/userModels");
 
-const userModels = require("../models/userModels")
+// Register controller
+const registerController = async (req, res) => {
+    try {
+        const { username, email, password, phone, address } = req.body;
 
-const registerController = async (req,res) => {}
-try{
-    const {username,email,password,phone,address} = req.body//
-      //validation
-      if(!username ||!email || !password || !address || !phone){
-        return res.status(500).send({
-            success:false,
-            message:"Please fill all the fields"
-        })
-      }
+        // Validation
+        if (!username || !email || !password || !address || !phone) {
+            return res.status(400).send({
+                success: false,
+                message: "Please fill all the fields",
+            });
+        }
 
-      //check user
-      const exisiting = await userModels.findOne({email})
-      if(exisiting){
-        return res.status(500).send({
-            success:false,
-            message:'Email Already Register please login'
-        })
+        // Check if user already exists
+        const existingUser = await userModels.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send({
+                success: false,
+                message: 'Email already registered, please login',
+            });
+        }
+
+        // Create new user
+        const user = await userModels.create({ username, email, password, phone, address });
+        res.status(201).send({
+            success: true,
+            message: 'User created successfully',
+            data: user, // You might want to send back the created user data
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in register API',
+            error,
+        });
     }
-    //create new user
-    const user = await userModels.create({username,email,password,phone,address})
-    res.status(201).send({
-        success:true,
-        message:'User Created Successfully',
-
-    })
-}catch (error){
-    console.log(error)
-    res.status(500).send({
-        success:false,
-        message:'Error in register api',
-        error
-    })
 };
 
-module.exports = {registerController}
+module.exports = { registerController };
