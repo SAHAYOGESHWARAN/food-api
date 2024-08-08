625cea5cc9590e33c9a836df9e6a27ce8fb1c335
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-08 10:44:36 +0530

    add food app master

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,26 @@
-const JWT = require('jsonwebtoken')
+const JWT = require("jsonwebtoken");
 
-module.exports =async (req,res,next) => {
-    try{
-        // get token
-        const token = req.headers["authorization"].split("")[1]
-       JWT.verify(token,process.env.SECRET_KEY,(err,decode) =>{
-        if(err){
-            return res.status(401).send({
-                success:false,
-                message:"Invalid token"
-            })
-            }else{
-                req.body.id = decode.id;
-                next();
-            }
-        })
-       
-    }catch (error){
-    console.log(error)
-   res.status(500).send({
-    success:false,
-    message:'Error in Auth API',
-    error:error.message
-   }) 
- }}
+module.exports = async (req, res, next) => {
+  try {
+    // get token
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          message: "Un-Authorize User",
+        });
+      } else {
+        req.body.id = decode.id;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Please provide Auth Token",
+      error,
+    });
+  }
+};
