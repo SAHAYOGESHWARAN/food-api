4cde19530922daa4d76e4ed9e7b1a106ecc06dc5
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-08 11:00:49 +0530

    add foodapp master

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,15 @@
 // GET USER INFO
 const getUserController = async  (req,res) => {
-    res.status(200).send("User Data");
-};
-
+  try {
+    //find user 
+    const user = await User.findById(req.user.id);
+} catch(error){
+    console.log(error)
+    res.status(500).send({
+        success:false,
+        message: 'Error fetching user info',
+        error
+    })
+}
+}
 module.exports = {getUserController};
