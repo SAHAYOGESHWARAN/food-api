9f87ad654e14c57260db584a929b462c8eb07162
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-08 11:03:18 +0530

    add foodapp master

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,6 +3,13 @@ const getUserController = async  (req,res) => {
   try {
     //find user 
     const user = await User.findById(req.user.id);
+    //validation
+    if(!user){
+        return res.status(404).send({
+            success:false,
+            message:"User not found"
+        })
+    }
 } catch(error){
     console.log(error)
     res.status(500).send({
