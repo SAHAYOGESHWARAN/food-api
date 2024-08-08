b0e611a6c40b219ecb3522d9a7587f6d6ed154a4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-08 11:24:49 +0530

    add food app master

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,22 +1,44 @@
 // GET USER INFO
+const userModels = require('../models/userModels');
 const getUserController = async  (req,res) => {
-  try {
-    //find user 
-    const user = await User.findById(req.user.id);
-    //validation
-    if(!user){
-        return res.status(404).send({
-            success:false,
-            message:"User not found"
-        })
-    }
-} catch(error){
-    console.log(error)
-    res.status(500).send({
-        success:false,
-        message: 'Error fetching user info',
-        error
-    })
-}
-}
-module.exports = {getUserController};
+    try {
+        // Use req.params.id
+        const userId = req.params.id;
+
+        if (!userId) {
+            return res.status(400).send({
+                success: false,
+                message: "User ID is required"
+            });
+        }
+
+        // Find user by ID
+        const user = await userModels.findById(userId);
+
+        // Validation
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "User not found"
+            });
+        }
+      // Handle password
+      user.password = undefined;
+
+      // Response
+      res.status(200).send({
+          success: true,
+          message: "User info",
+          data: user
+      });
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({
+          success: false,
+          message: 'Error fetching user info',
+          error
+      });
+  }
+};
+
+module.exports = { getUserController };
